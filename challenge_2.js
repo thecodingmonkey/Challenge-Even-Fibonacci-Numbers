03d9ac5a6aa499d7e824df877d0310fb635d5af0
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-15 11:11:39 -1000

    comment code to make it more readable

--- challenge_2.js
+++ challenge_2.js
@@ -1,22 +1,26 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
   // do your work here
-  var a=1, b=1;
+  var a= 1, b = 1; // first values of sequence
 
-  while (true) {
-    var nextVal = a+b;
-    if (nextVal > n) {
+  while (true) {  // no terminating condition, we terminate by returning
+                  // in the if statement below
+
+    var nextVal = a + b;  // calculate next value in sequence
+
+    if (nextVal > n) { // we are done if we pass n
       return sum;
     }
 
+    // check is nextVal is even, add to sum if it is
     if ((nextVal % 2) === 0) {
       sum = sum + nextVal;
     }
+
+    // shift current values to [b, nextVal] from [a, b]
     a = b;
     b = nextVal;
   }
 
-
-
-  return sum;
+  return sum;  // we never reach here
 };
