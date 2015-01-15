7ad851e59c8606bce91f591a8174ba4bf6b9342e
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-15 11:08:51 -1000

    implement summation function

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,22 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
   // do your work here
-  
+  var a=1, b=1;
+
+  while (true) {
+    var nextVal = a+b;
+    if (nextVal > n) {
+      return sum;
+    }
+
+    if ((nextVal % 2) === 0) {
+      sum = sum + nextVal;
+    }
+    a = b;
+    b = nextVal;
+  }
+
+
+
   return sum;
 };
