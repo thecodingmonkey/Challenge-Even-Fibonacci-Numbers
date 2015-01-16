7659266d00e82f3578d51853b320430d243fe4b0
Andrew Wong <coding.monkey.andrew@gmail.com>
2015-01-15 15:36:19 -1000

    ONE LINE!  (please don't do this if i ever code review your stuff)

--- challenge_2.js
+++ challenge_2.js
@@ -1,16 +1,3 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
-  var sum = 0;
-  // do your work here
-
-  var seq = Array.apply(null, new Array(50)).map(Number.prototype.valueOf,1);  //.fill(0, 0, 10);
-  seq = seq.map(function (currentValue, idx, arr) {
-    return (idx < 2) ? arr[idx] = 1 : arr[idx] = arr[idx-1] + arr[idx-2];
-  });
-
-  function reduceFibonacci(prev, cur) {
-    return ((cur % 2) === 0 && cur < n) ? prev + cur : prev;
-  }
-
-  return seq.reduce(reduceFibonacci, 0);
-
+  return Array.apply(null, new Array(50)).map(Number.prototype.valueOf,1).map(function (currentValue, idx, arr) { return (idx < 2) ? arr[idx] = 1 : arr[idx] = arr[idx-1] + arr[idx-2]; }).reduce(function (prev, cur) { return ((cur % 2) === 0 && cur < n) ? prev + cur : prev; }, 0);
 };
